c531a23690a979b1e22b38fbf72505d8513acc82
Владислав <vsvpiter27@gmail.com>
2022-01-15 15:34:04 +0300

    add new testcases

--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -20,6 +20,11 @@ func TestUnpack(t *testing.T) {
 		{input: `qwe\45`, expected: `qwe44444`},
 		{input: `qwe\\5`, expected: `qwe\\\\\`},
 		{input: `qwe\\\3`, expected: `qwe\3`},
+		{input: "нт2и", expected: "нтти"},
+		{input: "日本語", expected: "日本語"},
+		{input: "日本語3", expected: "日本語語語"},
+		{input: "ᚫ2ᛣ4", expected: "ᚫᚫᛣᛣᛣᛣ"},
+		{input: "!2rer", expected: "!!rer"},
 	}
 
 	for _, tc := range tests {
